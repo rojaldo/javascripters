0fe184c05da18bc5e3c5514c1ef5e07852690cac
Daniel Rojo <danielrojo@gmail.com>
2020-12-13 12:10:20 +0100

    trivial card processes answers

--- src/components/trivial/trivialcard.js
+++ src/components/trivial/trivialcard.js
@@ -4,21 +4,48 @@ import './trivialcard.css';
 export class TrivialCard extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {card: this.props.data}
     }
-    render() { 
-        const buttons = this.props.data.answers.map((answer, index) => <button type="button" class="btn btn-primary btn-block" key={index}>{answer}</button>);
-        return ( 
+
+    handleAnswer(index){
+        console.log('handleAnswer: '+index);
+        let temp  = this.state.card;
+        temp.setResponded();
+        temp.setAnswer(index);
+        this.setState({card: temp});
+    }
+
+    getClass(index){
+        if(this.state.card.isTheIncorrectAnswer(index)){
+            return "btn btn-danger btn-block";    
+        }
+        else if(this.state.card.isTheCorrectAnswer(index)) {
+            return "btn btn-success btn-block";    
+        } 
+        return "btn btn-secondary btn-block";
+    }
+
+    render() {
+        let buttons = <div></div>;
+        if(this.state.card.isResponded()){
+            buttons = this.state.card.answers.map((answer, index) =>
+            <button type="button" className={this.getClass(index)} disabled key={index}>{answer}</button>);
+        }else {
+            buttons = this.state.card.answers.map((answer, index) =>{
+                console.log(index);
+            return <button type="button" className="btn btn-primary btn-block" key={index} value={index} onClick={(event)=>{this.handleAnswer(event.target.value)}}>{answer}</button>});
+        }
+        return (
             <Fragment>
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">{this.props.data.question}</h4>
+                <div className="card">
+                    <div className="card-body">
+                        <h4 class="card-title">{this.state.card.question}</h4>
                         {buttons}
                     </div>
                 </div>
             </Fragment>
-         );
+        );
     }
 }
- 
+
 export default TrivialCard;
--- src/model/card.js
+++ src/model/card.js
@@ -6,5 +6,31 @@ export class Card{
         this.answers = json.incorrect_answers;
         this.answers.push(json.correct_answer);
         this.correctAnswer = json.correct_answer;
+        this.responded = false;
+        this.answerIndex = -1;
+    }
+
+    setResponded(){
+        this.responded = true;
+    }
+
+    isResponded(){
+        return this.responded;
+    }
+
+    setAnswer(index){
+        this.answerIndex = parseInt(index);
+    }
+
+    getAnswerIndex() {
+        return this.answerIndex;
+    }
+
+    isTheCorrectAnswer(index){
+        return this.answers[index] === this.correctAnswer;
+    }
+
+    isTheIncorrectAnswer(index){
+        return (index === this.answerIndex) && !this.isTheCorrectAnswer(index)
     }
 }
